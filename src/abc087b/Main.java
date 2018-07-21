65b07dccfa935a7cc87c3b1f53737826f53d4db8
HBonsai <hbonbyaku@gmail.com>
2018-07-21 20:04:33 +0900

    コンソールから入力を受け付ける部分を追加

--- src/abc087b/Main.java
+++ src/abc087b/Main.java
@@ -5,6 +5,15 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt(); // 500円玉
+		int b = sc.nextInt(); // 100円玉
+		int c = sc.nextInt(); // 50円玉
+		int x = sc.nextInt(); // 合計金額 50-20000
+
+		// 答え:合計金額がXになる方法はresult通り
+
+
+
 
 	}
 }
