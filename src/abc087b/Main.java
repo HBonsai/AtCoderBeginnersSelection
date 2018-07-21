c10bd2e5935657ab7cfa0818fb6390dff5506cc7
HBonsai <hbonbyaku@gmail.com>
2018-07-21 20:31:50 +0900

    Acceptされたので回答をコミット

--- src/abc087b/Main.java
+++ src/abc087b/Main.java
@@ -11,9 +11,19 @@ public class Main {
 		int x = sc.nextInt(); // 合計金額 50-20000
 
 		// 答え:合計金額がXになる方法はresult通り
-
-
-
-
+		// 500A + 100B + 50C = X
+		// 解くべき問題が間違っていた。まずすべての通りの合計金額を出し、それからXで絞り込めばいい。
+		int count = 0;
+		for (int ai = 0; ai <= a; ai++) {
+			for (int bi = 0; bi <= b; bi++) {
+				for (int ci = 0; ci <= c; ci++) {
+					int sum = ai * 500 + bi * 100 + ci * 50;
+					if (sum == x) {
+						count++;
+					}
+				}
+			}
+		}
+		System.out.println(count);
 	}
 }
