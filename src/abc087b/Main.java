8ed7be6e3b9b5450e21f55716b3160b16249c8e7
HBonsai <hbonbyaku@gmail.com>
2018-07-21 20:40:17 +0900

    解くのにかかった時間を追加

--- src/abc087b/Main.java
+++ src/abc087b/Main.java
@@ -1,7 +1,7 @@
 package abc087b;
 
 import java.util.Scanner;
-
+/** 解くのにかかった時間:約30分（コミット履歴より） */
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
