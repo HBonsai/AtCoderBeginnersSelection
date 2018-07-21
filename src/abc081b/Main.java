deb39ee5f0b56c8f0d74c6a80ba4d5840251f9d9
HBonsai <hbonbyaku@gmail.com>
2018-07-21 19:12:51 +0900

    ググったときの検索ワードと参考URLをコメントに追加

--- src/abc081b/Main.java
+++ src/abc081b/Main.java
@@ -6,14 +6,20 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
+		// ググった:「int 配列 java」 配列の宣言があやふや
+		// https://www.javadrive.jp/start/array/index2.html
 		int a[] = new int[n];
 
 		// nextIntは半角スペースで受け取っても普通によしなにしてくれるらしい
+		// （sysoutで実験して確かめた)
 		for (int i = 0; i < a.length; i++) {
 			a[i] = sc.nextInt();
 		}
 		// 奇数が出るまで割る
 		int count = 0;
+
+		// ググった:「for文 抜ける java」
+		// breakのラベリング https://www.javadrive.jp/start/break/index3.html
 		loop: while (true) {
 			for (int i = 0; i < a.length; i++) {
 				if (a[i]%2 ==1) {
